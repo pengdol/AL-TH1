b1755ac81c23a39ecef8138cab3c57918d3c04a0
pengdololololololololol <108115799+pengdol@users.noreply.github.com>
2023-03-03 18:51:28 +0900

    Update Week12.cpp

--- Week12.cpp
+++ Week12.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <stack>
 #include <queue>
+#include <algorithm>
 using namespace std;
 
 /*
@@ -67,6 +68,85 @@ int main(){
     }
   }
   printf("%d", s.top());
+  return 0;
+}
+
+4. sort 예제문제
+
+bool cmp(int left, int right){
+  return left > right;
+}
+int main(){
+  int data[10];
+  for(int i = 0; i < 10; i++)
+    scanf("%d", &data[i]);
+  sort(data, data + 10);
+  for(int i = 0; i < 10; i++)
+    printf("%d ", data[i]);
+  printf("\n");
+  sort(data, data + 10, cmp);
+  for(int i = 0; i < 10; i++)
+    printf("%d ", data[i]);
+  return 0;
+}
+
+5. 줄 세우기
+
+int main(){
+  double height[100001];
+  double temp[100001];
+  int n;
+  scanf("%d", &n);
+  for(int i = 0; i < n; i++){
+    scanf("%lf", &height[i]);
+    temp[i] = height[i];
+  }
+  sort(height, height + n);
+  int plus = 0;
+  for(int i = 0; i < n; i++){
+    if(height[i] != temp[i])
+      plus++;
+  }
+  printf("%d", plus);
+  return 0;
+}
+
+6. 팀원 집합
+
+int main(){
+  pair<int, int> p[100001] = {};
+  int n;
+  scanf("%d", &n);
+  for(int i = 0; i < n; i++){
+    cin >> p[i].first;
+    cin >> p[i].second;
+  }
+  sort(p, p + n);
+  for(int i = 0; i < n; i++){
+    printf("%d ", p[i].second);
+    if(p[i].first != p[i + 1].first)
+      printf("\n");
+  }
+  return 0;
+}
+
+7. 인터넷 강의
+
+int main(){
+  pair<int, int> p[100001];
+  int n;
+  scanf("%d", &n);
+  for(int i = 0; i < n; i++)
+    scanf("%d %d", &p[i].second, &p[i].first);
+  sort(p, p + n);
+  int start = 0, num = 0;
+  for(int i = 0; i < n; i++){
+    if(start <= p[i].second){
+      start = p[i].first;
+      num++;
+    }
+  }
+  printf("%d", num);
   return 0;
 }
 */
